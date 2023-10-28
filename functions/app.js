04e2cda22b1719030760ce8fa22c1f41c64aafc4
Aldi Andalan <andalanaldi@gmail.com>
2023-10-28 09:05:12 +0700

    feat: add hemet to prevent xss & clickjacking

--- functions/app.js
+++ functions/app.js
@@ -6,12 +6,28 @@ const authRouter = require('./routes/auth-route.js')
 const ipoRouter = require('./routes/ipo-route.js')
 const authMiddleware = require('./middleware/authentication-middleware.js')
 const cors = require('cors');
+const helmet = require('helmet');
 const Validator = require('express-validator');
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+// app.use(helmet())
+app.use(helmet({
+  contentSecurityPolicy: false, // Disable for now if not needed
+  frameguard: {
+    action: 'deny' // Prevent clickjacking
+  },
+  hidePoweredBy: true, // Remove the X-Powered-By header
+  hsts: {
+    maxAge: 15552000, // Enable HSTS
+    includeSubDomains: true
+  },
+  ieNoOpen: true, // Prevent IE from executing downloads in your site's context
+  noSniff: true, // Prevent browsers from MIME-sniffing a response away from declared content type
+  xssFilter: true // Prevent reflected XSS attacks
+}));
 app.use(databaseMiddleware)
 
 app.get('/', (req, res) => {
